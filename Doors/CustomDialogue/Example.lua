03bda11223d043edbf617a36cc5eaeeafa1c82b7
ThatOneGuyRuben <127993606+ThatOneGuyRuben@users.noreply.github.com>
2023-04-12 12:34:11 +0200

    Create Example.lua

---
+++ Doors/CustomDialogue/Example.lua
@@ -0,0 +1,7 @@
+local Creator = loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatOneGuyRuben/Stuff/main/Scripts/Test.lua"))()
+
+Creator.CustomDialogue({
+    DialogueText = {"You can change the text", "You can also add more lines", "Have fun using this!", "Made by That_oneGuyRuben on Pastebin or ThatOneGuyRuben on Github"}, --Change this to your liking.
+    Color = "Blue", --Change Blue to Yellow for Curious Light.
+    DeathCause = "Example" --Change Example to anything you want
+})
